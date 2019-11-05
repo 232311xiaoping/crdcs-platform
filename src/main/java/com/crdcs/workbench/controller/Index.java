5a4eba0493036e8b0e55a56da484814368eee2ca
232311xiaoping <232311xiaoping@163.com>
2019-11-05 17:01:13 +0800

    测试hello world

--- src/main/java/com/crdcs/workbench/controller/Index.java
+++ src/main/java/com/crdcs/workbench/controller/Index.java
@@ -1,12 +1,27 @@
 package com.crdcs.workbench.controller;
 
+import io.netty.handler.codec.http.HttpResponse;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class Index {
-    @RequestMapping("/")
-    public String index(){
+    @RequestMapping(value = { "/**/*.htm", "/**/*.html","/" })
+    public String index(HttpServletRequest request, HttpServletResponse response){
+
+        HttpSession session = request.getSession();
+
+        Cookie[] cookies = request.getCookies();
+
+        response.addCookie(cookies[0]);
+
+
         return "index";
     }
 }
